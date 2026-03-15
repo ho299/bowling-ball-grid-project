22c79fac4fc9601fd030951d2075c0fb5d3ca247
Josh Ho <jwho@wisc.edu>
2026-03-15 16:49:33 -0500

    adding back the other routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,20 +5,20 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-console.log("Backend Starting...little")
+console.log("Backend Starting...")
 // const init = async() => {
     //page routing below:
     const specRoutes = require('./routes/specs_routes');
     app.use('/api/specs', specRoutes);
     
-    // const coverstockRoutes = require('./routes/coverstock_routes');
-    // app.use('/api/coverstocks', coverstockRoutes);
+    const coverstockRoutes = require('./routes/coverstock_routes');
+    app.use('/api/coverstocks', coverstockRoutes);
     
-    // const coreRoutes = require('./routes/core_routes');
-    // app.use('/api/cores', coreRoutes);
+    const coreRoutes = require('./routes/core_routes');
+    app.use('/api/cores', coreRoutes);
     
-    // const ballRoutes = require('./routes/ball_routes');
-    // app.use('/api/ball', ballRoutes);
+    const ballRoutes = require('./routes/ball_routes');
+    app.use('/api/ball', ballRoutes);
 
     app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -30,6 +30,6 @@ console.log("Backend Starting...little")
 // };
 
 // init();
-
+console.log("Backend Loaded...")
 
 module.exports = app;
