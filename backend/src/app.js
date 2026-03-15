8ec4c95de3f8219e586929ee483e32f6ed6ee249
Josh Ho <jwho@wisc.edu>
2026-03-15 16:10:52 -0500

    route name change

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,23 @@
 
 // app.js
 const express = require('express');
+const coverstockRoutes = require('./routes/coverstock_routes');
+const ballRoutes = require('./routes/ball_routes');
+const coreRoutes = require('./routes/core_routes');
+const specRoutes = require('./routes/specs_routes');
+
 const app = express();
-const specs = require('./routes/specs_routes');
-const coverstocks = require('./routes/coverstock_routes');
-const ball = require('./routes/ball_routes');
-const core = require('./routes/core_routes');
 const port = 3001;
 
 //page routing below:
-app.use('/api/specs', specs);
-app.use('/api/coverstocks', coverstocks);
-app.use('/api/cores', core);
-app.use('/api/ball', ball);
+app.use('/api/specs', specRoutes);
+app.use('/api/coverstocks', coverstockRoutes);
+app.use('/api/cores', coreRoutes);
+app.use('/api/ball', ballRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`Backend Open on Port: ${port}`);
