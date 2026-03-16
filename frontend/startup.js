5f5faf463b58206ed20c860a04056b68f6faa1b5
Josh Ho <jwho@wisc.edu>
2026-03-15 19:57:11 -0500

    I think this is causing problems

--- frontend/startup.js
+++ frontend/startup.js
@@ -10,9 +10,9 @@ app.get('/', (req, res) => {
   res.send('Hello World from a Docker container!');
 });
 
-app.get('/homepage', (req, res) => {
-    res.sendFile(path.join(__dirname, 'homepage.html'));
-});
+// app.get('/homepage', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'homepage.html'));
+// });
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
