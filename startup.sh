07d618f7a366561002e2f1d6549ea35e04859188
Josh Ho <jwho@wisc.edu>
2026-03-29 19:35:32 -0500

    updated what I did for the containers

--- startup.sh
+++ startup.sh
@@ -1,2 +1,7 @@
-./startupFrontEnd.sh
-./startupBackEnd.sh
+docker pull postgres
+docker network create bowlingBallAppProject
+docker build -t postgres:1.0.0 .
+docker run -i --rm --network=bowlingBallAppProject \
+ -p 5432:5432 \
+ -e dev=dev \
+ --name=postgres postgres:1.0.0
--- startupBackEnd.sh
+++ startupBackEnd.sh
@@ -3,4 +3,8 @@ docker network create bowlingBallAppProject
 docker build -t backend:1.0.0.beta ./backend
 docker run -i --rm --network=bowlingBallAppProject \
  -p 3000:3000 \
+ -e DB_NAME=bowling \
+ -e DB_HOST=postgres \
+ -e DB_USER=docker_user \
+ -e DB_PASSWORD=mysecretpassword \
  --name=backend backend:1.0.0.beta
